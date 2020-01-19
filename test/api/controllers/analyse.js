79fe5bc3371dee357813c1829fb151c1847487a3
Andy <aw087ok@gmail.com>
2020-01-19 00:06:02 +0000

    Added empty test case definitions for POST /analyse/demo

--- test/api/controllers/analyse.js
+++ test/api/controllers/analyse.js
@@ -41,5 +41,32 @@ describe('controllers', function() {
                   });
               });        
         })
+
+        describe('POST /analyse/demo', function() {
+
+          it('should be able to send an audio file and get a classified emotion back', function(done) {
+            done();
+          });
+
+          it('should give error if video file was sent', function(done) {
+            done();
+          });
+
+          it('should give error if other file types were sent', function(done) {
+            done();
+          });
+
+          it('should give error if no file given', function(done) {
+            done();
+          });
+
+          it('should give error if invalid demoCode givne', function(done) {
+            done();
+          });
+
+          it('should give error if no demoCode given', function(done) {
+            done();
+          });
+        })
     })
 });
