c89c040ff39a5c25cbab8b8bbfd5050d5c54fb6b
Andy <aw087ok@gmail.com>
2020-02-02 17:03:23 +0000

    Controller and Swagger definitions for API endpoint implemented

--- api/controllers/analyse.js
+++ api/controllers/analyse.js
@@ -1,10 +1,17 @@
 'use strict';
 
-// Controller for handling interaction between API endpoint and SER system
+// Controllers for handling interaction between API endpoint and Speech Emotion Recognition (SER) system
 
-const util = require('util');
+const SER_MODEL_PATH = `${__dirname}/../models/classifier.joblib`;
 
-function analyse(req, res) {
+const util = require('util'),
+    fileHelpers = require('../helpers/fileConverter'),
+    pythonRunner = require('../helpers/pythonRunner');
+
+/**
+ * Sends a hello world message back to the user to test the API.
+ */
+function analyseHelloWorld(req, res) {
     // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
     var name = req.swagger.params.name.value || 'stranger';
     var hello = util.format('Hello, %s!', name);
@@ -13,6 +20,87 @@ function analyse(req, res) {
     res.json(hello);
 }
 
+const DEMO_CODE_VALIDATION = 'Imicrowavecereal'
+/**
+ * Send an audio file with speech and get the most probable emotion 
+ * shown back (done via Support Vector Machine Classification)
+ */
+function analyseDemo(req, res) {
+    const _file = req.swagger.params.file.value,
+        _demoCode = req.swagger.params.demoCode.value;
+
+    // Used to prevent spamming of invalid requests
+    if (_demoCode != DEMO_CODE_VALIDATION) {
+        return res.status(400).send({
+            errorCode: 400,
+            message: 'Invalid demo code validation string provided.'
+        });
+    }
+
+    // Only accept one file at a time
+    else if (Array.isArray(_file)) {
+        return res.status(400).send({
+            errorCode: 400,
+            message: 'Invalid file providied, API only supports one audio file at a time.'
+        });
+    }
+
+    // Ensure given file is an audio file
+    else if (!_file.mimetype.includes('audio')) {
+        return res.status(400).send({
+            errorCode: 400,
+            message: 'Invalid file providied, not an audio file.'
+        });
+    };
+
+    // Path to temporarily stored audio file for Python script to be performed on
+    let customFilePath = null;
+
+    // Writing into an audio file for the Python script to process
+    return fileHelpers.write(_file.buffer)
+    .then(_customFilePath => {
+        customFilePath = _customFilePath;
+
+        // Perform the SER via the Python script
+        return pythonRunner.run(`${__dirname}/../models/demo.py`, [customFilePath, SER_MODEL_PATH])
+        .then(classification => {
+            console.log(classification);
+
+            // Audio file now redundant; removing the audio file to prevent it from taking storage space
+            return fileHelpers.remove(customFilePath)
+            .then(() => {
+
+                // Send final output
+                res.status(200).send({
+                    emotion: classification
+                });
+            })
+        })
+    })
+    .catch(err => {
+        // Error occurred, use console.log to find issue
+        console.log(err);
+
+        // Ensure the temporary file is removed
+        if (customFilePath != null) {
+            return fileHelpers.remove(customFilePath)
+            .then(() => {
+
+                res.status(400).send({
+                    errorCode: 400,
+                    message: err
+                });
+            })
+        } else {
+            res.status(400).send({
+                errorCode: 400,
+                message: err
+            });
+        };
+    })
+}
+
 module.exports = {
-    analyse: analyse
+    analyse: analyseHelloWorld,
+    analyseDemo: analyseDemo
 }
