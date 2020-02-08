e81bd0fec0f9c098f273eae0eb9bbc89614f59c8
Andy <aw087ok@gmail.com>
2020-02-08 16:26:47 +0000

    Implemented controller to implement API endpoint to output emotional statistics

--- api/controllers/analyse.js
+++ api/controllers/analyse.js
@@ -21,6 +21,86 @@ function analyseHelloWorld(req, res) {
 }
 
 /**
+ * Send an audio file with speech and get the emotional statistics 
+ * shown back (done via Support Vector Machine Classification)
+ */
+function analyseAll(req, res) {
+    const _file = req.swagger.params.file.value;
+
+    // Only accept one file at a time
+    if (Array.isArray(_file)) {
+        return res.status(400).send({
+            errorCode: 400,
+            message: 'Invalid file providied, API only supports one audio file at a time.'
+        });
+    }
+
+    // Ensure given file is an audio file
+    else if (!_file.mimetype.includes('audio')) {
+        return res.status(400).send({
+            errorCode: 400,
+            message: 'Invalid file providied, not an audio file.'
+        });
+    };
+
+    // Path to temporarily stored audio file for Python script to be performed on
+    let customFilePath = null;
+
+    // Writing into an audio file for the Python script to process
+    return fileHelpers.write(_file.buffer)
+    .then(_customFilePath => {
+        customFilePath = _customFilePath;
+
+        // Perform the SER via the Python script
+        return pythonRunner.run(`${__dirname}/../models/demo.py`, [customFilePath, SER_MODEL_PATH])
+        .then(_emotionalStatistics => {
+            const emotionalStatistics = JSON.parse(_emotionalStatistics).data;
+            let result = [];
+
+            // Wrap result into the appropriate output schema
+            // i.e. [{ emotion: '', probability: 0 }]
+            emotionalStatistics.forEach(arr => {
+                result.push({
+                    emotion: arr[0],
+                    probability: arr[1]
+                });
+            });
+
+            // Audio file now redundant; removing the audio file to prevent it from taking storage space
+            return fileHelpers.remove(customFilePath)
+            .then(() => {
+
+                // Send final output
+                res.status(200).send({
+                    emotions: result
+                });
+            })
+        })
+    })
+    .catch(err => {
+        // Error occurred, use console.log to find issue
+        console.log(err);
+
+        // Ensure the temporary file is removed
+        if (customFilePath != null) {
+            return fileHelpers.remove(customFilePath)
+            .then(() => {
+
+                res.status(400).send({
+                    errorCode: 400,
+                    message: err
+                });
+            })
+        } else {
+            res.status(400).send({
+                errorCode: 400,
+                message: err
+            });
+        };
+    })
+}
+
+/**
  * Send an audio file with speech and get the most probable emotion 
  * shown back (done via Support Vector Machine Classification)
  * 
@@ -105,6 +185,7 @@ function analyseDemo(req, res) {
 */
 
 module.exports = {
-    analyse: analyseHelloWorld
+    analyse: analyseHelloWorld,
+    analyseAll: analyseAll
     // analyseDemo: analyseDemo
 }
