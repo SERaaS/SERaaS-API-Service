13c71e23ae75867bdf5a11d2cec0c1d74d7f9b52
Andy <aw087ok@gmail.com>
2020-02-17 20:30:37 +0000

    File helper for audio files duration splitting

--- api/helpers/fileConverter.js
+++ api/helpers/fileConverter.js
@@ -1,5 +1,9 @@
 const fs = require('fs').promises,
-    uuid = require('uuid/v1');
+    uuid = require('uuid/v1'),
+
+    // Utilities for executing native Terminal commands
+    util = require('util'),
+    exec = util.promisify(require('child_process').exec);
 
 /**
  * Handles conversion of a file buffer to an audio .wav file, to allow Python 
@@ -20,6 +24,29 @@ function audioFileConversion(fileBuffer) {
 }
 
 /**
+ * Splits the given audio file into multiple ones based on the given
+ * duration desired for each file (in seconds).
+ */
+function splitAudioFile(filePath, durationPerFile) {
+    const fileType = filePath.slice(-4),
+        _filePath = filePath.slice(0, -4); // Without the file type
+
+    return new Promise((resolve, reject) => {
+        exec(`ffmpeg -i ${filePath} -f segment -segment_time ${durationPerFile} -c copy ${_filePath}%04d${fileType} -hide_banner`)
+        .then(res => {
+
+            // Retrieving the resulting file paths via [filePath]0000[fileType]
+            // e.g. speech0001.wav -> match
+            let out = res.stderr.match(new RegExp(`${_filePath}\\d{4}${fileType}`, 'g'));
+            resolve(out);
+        })
+        .catch(err => {
+            reject(err);
+        })
+    });
+};
+
+/**
  * Removes an existing tmp/ file after being created.
  */
 function removeTemporaryFile(filePath) {
@@ -36,5 +63,6 @@ function removeTemporaryFile(filePath) {
 
 module.exports = {
     write: audioFileConversion,
-    remove: removeTemporaryFile
+    remove: removeTemporaryFile,
+    split: splitAudioFile
 };
